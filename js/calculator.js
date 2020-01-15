9bb5027a81749e6120c381cdbed24dcbda9f843c
Ashish0077 <ashish007722@gmail.com>
2020-01-15 14:00:28 +0530

    intiail javascript file
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

---
+++ js/calculator.js
@@ -0,0 +1,38 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value) {
+    if(isNaN(value)) {
+        handleSymbol(value)
+    } else {
+        handleNumber(value)
+    }
+    screen.innerText = buffer;
+}
+
+function handleSymbol(symbol) {
+    if(symbol == 'C') {
+        buffer = 0;
+        runningTotal = 0;
+    }
+
+}
+
+function handleNumber(numberString) {
+    if(buffer == "0")
+        buffer = numberString;
+    else 
+        buffer += numberString;
+}
+
+function init() {
+    document.querySelector('.calc-buttons')
+        .addEventListener('click', function(event) {
+            buttonClick(event.target.innerText);
+        })
+}
+
+init();
