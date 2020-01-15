92be56f63d2834e029ea8397dcd52551cb31bf07
Ashish0077 <ashish007722@gmail.com>
2020-01-15 14:54:39 +0530

    update functionality
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,6 @@
 let runningTotal = 0;
 let buffer = "0";
-let previousOperator;
+let previousOperator = null;
 
 const screen = document.querySelector('.screen');
 
@@ -14,11 +14,68 @@ function buttonClick(value) {
 }
 
 function handleSymbol(symbol) {
-    if(symbol == 'C') {
-        buffer = 0;
-        runningTotal = 0;
+    
+    switch (symbol) {
+        case 'C': 
+                    buffer = 0;
+                    runningTotal = 0;
+                    break;
+        case '=':
+                    if(previousOperator == null) {
+                        return;
+                    }
+                    flushOperation(parseInt(buffer));
+                    previousOperator = null;
+                    buffer = runningTotal;
+                    runningTotal = 0;
+                    break;
+        case '←':
+                    if(buffer.length == 1) {
+                        buffer = "0";
+                    } else {
+                        buffer = buffer.substring(0, buffer.length - 1);
+                    }
+                    break;
+        case '+':
+        case '-':
+        case '×':
+        case '÷':
+                    handleMath(symbol);
+                    break;
     }
 
+}
+
+function handleMath(symbol) {
+    if (buffer == '0') {
+        //do nothing
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if(runningTotal == 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = symbol;
+
+    buffer = 0;
+}
+
+function flushOperation(intBuffer) {
+    if(previousOperator == '+') {
+        runningTotal += intBuffer;
+    } else if (previousOperator == '-') {
+        runningTotal -= intBuffer;
+    } else if (previousOperator == '×') {
+        runningTotal *= intBuffer;
+    } else {
+        runningTotal /= intBuffer;
+    }
+
+    console.log(runningTotal);
 }
 
 function handleNumber(numberString) {
